253f6d3be0c7e758840d8eccf5cae503322d20a9
Tobias Kalbitz <tobias.kalbitz@gmail.com>
2025-09-09 17:32:25 +0200

    Remove unneeded files.

--- src/uc_ball_hyp_generator/deploy.py
+++ src/uc_ball_hyp_generator/deploy.py
@@ -5,10 +5,7 @@ import torch
 import torch.onnx
 
 import uc_ball_hyp_generator.models as models
-from uc_ball_hyp_generator.config import image_dir, patch_height, patch_width, testset_csv_collection
-from uc_ball_hyp_generator.csv_label_reader import load_csv_collection
-from uc_ball_hyp_generator.dataset_handling import create_dataset
-from uc_ball_hyp_generator.scale import unscale_x, unscale_y
+from uc_ball_hyp_generator.config import patch_height, patch_width
 
 # Force CPU for deployment
 device: torch.device = torch.device("cpu")
@@ -24,45 +21,12 @@ if len(sys.argv) == 2:
 
 print(f"Weight file: {weight_file}")
 
-
-def create_representative_dataset():
-    png_files = {f.name: str(f) for f in image_dir.glob("**/*.png")}
-
-    test_img, test_labels, skipped_testset = load_csv_collection(testset_csv_collection, png_files)
-
-    print(f"Testset contains {len(test_img)} and we removed {skipped_testset} balls.")
-
-    ds = create_dataset(test_img, test_labels, 1, trainset=False)
-
-    def representative_dataset():
-        for data in ds.take(1000):
-            a = data[0]
-            yield [a]
-
-    return representative_dataset()
-
-
-def distance_loss(y_pred, y_true):
-    """PyTorch version of distance loss."""
-
-    def _logcosh(x):
-        return x + torch.nn.functional.softplus(-2.0 * x) - torch.log(torch.tensor(2.0))
-
-    y_t = torch.stack([unscale_x(y_true[:, 0]), unscale_y(y_true[:, 1])], dim=1)
-    y_p = torch.stack([unscale_x(y_pred[:, 0]), unscale_y(y_pred[:, 1])], dim=1)
-    r = _logcosh(y_p - y_t)
-    e = torch.exp(3.0 / y_true[:, 2])
-    r = r * e.unsqueeze(1)
-    return torch.mean(r)
-
-
 model = models.create_network_v2(patch_height, patch_width)
 model.load_state_dict(torch.load(weight_file, map_location=device))
 model.to(device)
 model.eval()
 
 
-tflite_file = deploy_dir + "/" + detector_name + ".tflite"
 model_file = deploy_dir + "/" + detector_name + ".pth"
 
 with open(deploy_dir + "/" + detector_name + ".txt", "w") as fp:
